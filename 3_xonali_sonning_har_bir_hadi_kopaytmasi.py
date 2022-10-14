1676124a2d8545593887703f7d6697e52ee8b896
Zarifxon <zarifsf@mail.ru>
2022-10-14 11:07:40 +0500

    14.10.2022 Birinchi ish
    14.10.2022 Birinchi ish DK

---
+++ 3_xonali_sonning_har_bir_hadi_kopaytmasi.py
@@ -0,0 +1,11 @@
+x=int(input("Uch xonali sonni kiriting:\n>>>"))
+a=x//100
+b=x%100
+c=b//10
+d = b%10
+print("Birinchi hadi: ", a)
+print("Ikkinchi hadi: ", c)
+print("Uchunchi hadi: ", d)
+
+e = a*c*d
+print("Har bir hadining ko'paytmasi: ", e)
